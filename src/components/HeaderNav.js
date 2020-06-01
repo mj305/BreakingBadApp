9189094e54d5f22261baa8198e14cbf68ed130cc
mj305 <maria.j.beckles@gmail.com>
2020-05-31 21:57:01 -0400

    added nav bar with search bar and search bar in body

---
+++ src/components/HeaderNav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+
+const HeaderNav = () => {
+
+  return(
+
+      <>
+        <Navbar expand="lg" variant="light" bg="light">
+        <Navbar.Brand href="#"> Breadking Bad </Navbar.Brand>
+          <Form inline>
+            <FormControl type="text" placeholder="Search Character" className=" mr-sm-2" />
+            <Button type="submit">Find</Button>
+          </Form>
+        </Navbar>
+      </>
+  );
+};
+
+export default HeaderNav;
