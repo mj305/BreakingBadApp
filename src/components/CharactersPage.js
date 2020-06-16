4c950dac63a26d58231ba8a58658b8fe24310a11
mj305 <maria.j.beckles@gmail.com>
2020-06-16 16:33:27 -0400

    styled individual character page in grid

--- src/components/CharactersPage.js
+++ src/components/CharactersPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { withRouter, useParams } from "react-router-dom";
 import SearchBarComponent from './SearchBarComponent';
+import Footer from '../components/FooterComponent';
 
 const CharacterPage = () => {
 
@@ -20,25 +21,23 @@ const CharacterPage = () => {
   return( 
 
     <>
-    <div className="characters-page-container">
+     <SearchBarComponent />
+      <div className="character-page-container" >
+          <div className="character-page-title-name" >
+            <h1 className="character-page-title" >{charactersInfo[0].name}</h1>
+            <img className="character-page-picture" src={charactersInfo[0].img} />
+          </div>
 
-       <SearchBarComponent />
-
-        <div className="characters-page-cards-container" />
-
-            <div className="characters-page-cards-title-name" >
-              <h1>{charactersInfo[0].name}</h1>
-              <img src={charactersInfo[0].img} />
-            </div>
-
-            <div className="characters-page-cards-descriptions" >
-              <p>Alias: {charactersInfo[0].nickname}</p>
-              <p>Occupation: {charactersInfo[0].occupation}</p>
-              <p>Status: {charactersInfo[0].status}</p>
-              <p>Actor Name: {charactersInfo[0].portrayed}</p>
-            </div>
-
-        </div>
+          <div className="character-page-cards-descriptions" >
+            <p>Alias: {charactersInfo[0].nickname}</p>
+            <p>Occupation: {charactersInfo[0].occupation}</p>
+            <p>Status: {charactersInfo[0].status}</p>
+            <p>Actor Name: {charactersInfo[0].portrayed}</p>
+          </div>
+      </div> 
+      <div  className="footer-container"  >
+       <Footer />
+      </div>       
     </>
 
     )
